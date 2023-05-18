3ae86cf4ceea7dad72371d7961be9a56c8b48cd1
Antoine Meloche <53838501+Antoine-Meloche@users.noreply.github.com>
2023-05-17 21:57:34 -0400

    Start updates to GNOME 44

--- shortcutbutton@antoine-meloche.github.com/prefs.js
+++ shortcutbutton@antoine-meloche.github.com/prefs.js
@@ -21,104 +21,56 @@ class UserCommandPrefsWidget extends Adw.PreferencesGroup {
       label: "Command to execute: "
     });
     let commandEntry = new Gtk.Entry();
-    // commandEntry.connect('changed', (w) => {
-    //   log(w.get_text());
-    // })
 
     let hBoxCommand = new Gtk.Box();
     hBoxCommand.set_orientation(Gtk.Orientation.HORIZONTAL);
-    //if (imports.gi.versions.Gtk.startsWith("3")) {
-    if (true) {
-      hBoxCommand.add(commandLabel);
-      hBoxCommand.add(commandEntry);
-    }
-    else {
-      hBoxCommand.append(commandLabel);
-      hBoxCommand.append(commandEntry);
-    }
+    hBoxCommand.append(commandLabel);
+    hBoxCommand.append(commandEntry);
 
     settings.bind('command', commandEntry, 'text', Gio.SettingsBindFlags.DEFAULT);
 
-    let filter = new Gtk.FileFilter();
-    filter.add_mime_type('image/png');
-    filter.add_mime_type('image/svg+xml');
-    let iconChooser = new Gtk.FileChooserDialog({
-      action: Gtk.FileChooserAction.OPEN,
-      filter: filter,
-      select_multiple: false,
-      title: 'Select Icon',
-    })
-    let defaultFolder = Gio.File.new_for_path(GLib.build_filenamev([GLib.get_home_dir(), "Pictures"]));
-    iconChooser.set_current_folder(defaultFolder);
-    iconChooser.add_button('Cancel', Gtk.ResponseType.CANCEL);
-    iconChooser.add_button('OK', Gtk.ResponseType.OK);
-
-    // let iconChooser = Gtk.FileChooserButton.new('Pick an icon', Gtk.FileChooserAction.OPEN);
-    // let iconPreview = new Gtk.Image();
-    // iconChooser.connect('file-set', () => {
-    //   let fileName = iconChooser.get_filename();
-    //   iconPreview.set_from_file(fileName);
-    // })
-
-
-    // iconChooser.set_preview_widget(iconPreview);
-    // iconChooser.set_use_preview_label(false);
-    // iconChooser.set_title('Icon to use');
-    // let imageFilter = Gtk.FileFilter();
-    // imageFilter.add_pixbuf_formats();
-    // iconChooser.add_filter(imageFilter);
-    // let defaultFolder = GLib.build_filenamev([GLib.get_home_dir(), "Pictures"]);
-    // iconChooser.set_current_folder(defaultFolder);
-
-    settings.bind('icon', iconChooser, 'text', Gio.SettingsBindFlags.DEFAULT);
-
-    // iconChooser.connect('update-preview', this.updatePreviewCb);
-
-    // let iconLabel = new Gtk.Label({
-    //   label: "Icon to use: "
-    // });
-    // let iconEntry = new Gtk.Entry();
-    // let iconPreview = new St.Icon({
-    // 	icon_name: settings.get_value('icon').get_string()[0],
-    // 	style_class: 'system-status-icon',
-    // })
-
-    // settings.bind('icon', commandEntry, 'text', Gio.SettingsBindFlags.DEFAULT);
+    let iconLabel = new Gtk.Label({
+      label: "Icon: "
+    });
+    let iconPreview = new Gtk.Image();
+    let iconButton = new Gtk.Button({
+      label: "Select icon"
+    });
+    iconButton.connect('clicked', () => {
+      let fileChooser = new Gtk.FileChooserDialog({
+        title: "Select icon",
+        action: Gtk.FileChooserAction.OPEN,
+        modal: true
+      });
+      fileChooser.add_button("Cancel", Gtk.ResponseType.CANCEL);
+      fileChooser.add_button("Select", Gtk.ResponseType.OK);
+      fileChooser.connect('response', (dialog, response) => {
+        if (response == Gtk.ResponseType.OK) {
+          let filename = fileChooser.get_filename();
+          if (filename != null && filename != "") {
+            this.updatePreviewCb(filename);
+            fileChooser.destroy();
+          }
+        }
+        fileChooser.destroy();
+      });
+      fileChooser.show();
+    });
 
     let hBoxIcon = new Gtk.Box();
     hBoxIcon.set_orientation(Gtk.Orientation.HORIZONTAL);
-    //if (imports.gi.versions.Gtk.startsWith("3")) {
-    if (true) {
-      // hBoxIcon.add(iconLabel);
-      // hBoxIcon.add(iconEntry);
-      // hBoxIcon.add(iconPreview);
-      hBoxIcon.add(iconChooser);
-      hBoxIcon.add(iconChooser);
-    }
-    else {
-      // hBoxIcon.append(iconLabel);
-      // hBoxIcon.append(iconEntry);
-      // hBoxIcon.append(iconPreview);
-      hBoxIcon.append(iconChooser);
-      hBoxIcon.append(iconChooser);
-    }
+    hBoxIcon.append(iconLabel);
+    hBoxIcon.append(iconPreview);
+    hBoxIcon.append(iconButton);
 
     let vBox = new Gtk.Box();
     vBox.set_orientation(Gtk.Orientation.VERTICAL);
-    //if (imports.gi.versions.Gtk.startswith("3")) {
-    if (true) {
-      vBox.add(hBoxCommand);
-      vBox.add(hBoxIcon);
-    } else {
-      vBox.append(hBoxIcon);
-    }
-
+    vBox.append(hBoxIcon);
     this.add(vBox);
 
   }
   
-  updatePreviewCb(fileChooser) {
-    let iconPath = fileChooser.get_filename();
+  updatePreviewCb(iconPath) {
     let pixbuf = GdkPixbuf.Pixbuf.new_from_file(iconPath);
     this.iconPreview.set_from_pixbuf(pixbuf);
 
